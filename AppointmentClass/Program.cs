e1d5881441781f1e8959b8e6b4244eb6fab047db
Nuria Siddiqa <nuri20024@gmail.com>
2025-02-11 15:01:51 -0500

    Add protypes for two functions

--- AppointmentClass/Program.cs
+++ AppointmentClass/Program.cs
@@ -1,4 +1,11 @@
 ﻿public class Appointment {
+   public int findFreeBlock(int period, int duration) {
 
+   }
+
+   public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+
+   }
 
 }
+
